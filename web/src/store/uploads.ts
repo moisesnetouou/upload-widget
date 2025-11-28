8b1f90fb2c95c11ebbc93a0a689966ffbb29da69
Moisés <moisesnetored@gmail.com>
2025-11-28 00:04:01 -0400

    feat: add immer with zustand

--- web/src/store/uploads.ts
+++ web/src/store/uploads.ts
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import { enableMapSet } from "immer";
+import {immer} from 'zustand/middleware/immer'
 
 export interface Upload {
   name: string;
@@ -10,7 +12,9 @@ type UploadState = {
   addUploads: (files: File[]) => void
 }
 
-export const useUploads = create<UploadState>((set, get) => {
+enableMapSet()
+
+export const useUploads = create<UploadState, [['zustand/immer', never]]>(immer((set) => {
   function addUploads(files: File[]) {
     for (const file of files) {
       const uploadId = crypto.randomUUID()
@@ -21,9 +25,7 @@ export const useUploads = create<UploadState>((set, get) => {
       }
 
       set((state) => {
-        return {
-          uploads: state.uploads.set(uploadId, upload)
-        }
+          state.uploads.set(uploadId, upload)
       })
     }
   }
@@ -32,4 +34,4 @@ export const useUploads = create<UploadState>((set, get) => {
     uploads: new Map(),
     addUploads
   }
-})
+}))
