a61057ed6e1e16b30c970cfcafbab30382373e02
Moisés <moisesnetored@gmail.com>
2025-11-28 00:13:50 -0400

    feat: files upload

--- web/src/store/uploads.ts
+++ web/src/store/uploads.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { enableMapSet } from "immer";
 import {immer} from 'zustand/middleware/immer'
+import { uploadFileToStorage } from "../http/upload-file-to-storage";
 
 export interface Upload {
   name: string;
@@ -14,7 +15,15 @@ type UploadState = {
 
 enableMapSet()
 
-export const useUploads = create<UploadState, [['zustand/immer', never]]>(immer((set) => {
+export const useUploads = create<UploadState, [['zustand/immer', never]]>(immer((set, get) => {
+  async function processUpload(uploadId: string) {
+    const upload = get().uploads.get(uploadId)
+
+    if(!upload) return
+
+    await uploadFileToStorage({ file: upload.file })
+  }
+
   function addUploads(files: File[]) {
     for (const file of files) {
       const uploadId = crypto.randomUUID()
@@ -27,6 +36,8 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(immer(
       set((state) => {
           state.uploads.set(uploadId, upload)
       })
+
+      processUpload(uploadId)
     }
   }
 
