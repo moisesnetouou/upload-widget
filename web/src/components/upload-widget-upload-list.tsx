8fb1b21a1642be08952b1dd346d85fe5ef25e022
Moisés <moisesnetored@gmail.com>
2025-11-27 22:55:34 -0400

    feat: visual component states

--- web/src/components/upload-widget-upload-list.tsx
+++ web/src/components/upload-widget-upload-list.tsx
@@ -1,14 +1,24 @@
 import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
 
 export function UploadWidgetUploadList(){
+  const isUploadListEmpty = false
+
   return(
     <div className="px-3 flex flex-col gap-3">
       <span className="text-xs font-medium">Uploaded files {' '}
         <span className="text-zinc-400">(2)</span>
       </span>
 
-      <UploadWidgetUploadItem />
-      <UploadWidgetUploadItem />
+      {isUploadListEmpty ? (
+        <span className="text-xs text-zinc-400">
+          No uploads added
+        </span>
+      ): (
+        <div>
+          <UploadWidgetUploadItem />
+          <UploadWidgetUploadItem />
+        </div>
+      )}
     </div>
   )
 }
